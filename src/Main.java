20837942c2ebb85a9719650775fe0e18871778cc
oskalbarczyk <106467480+oskalbarczyk@users.noreply.github.com>
2023-06-15 21:01:04 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 /**
  * Simple Tic-Tac-Toe in Console
  *
+ * This program allows two players to play Tic-Tac-Toe in the console.
+ * The players take turns selecting fields on the board, and the game
+ * checks for winning conditions after each move.
  *
  * @author Oskar Kalbarczyk
- * https://github.com/oskalbarczyk
+ * GitHub: https://github.com/oskalbarczyk
  *
  * @version 1
  */
@@ -14,7 +17,7 @@ public class Main {
 
     public static Field [] board = new Field[9];
     public static void main(String[] args) {
-
+         // Welcome message and author information
         System.out.println("Welcome to Simple Tic-Tac-Toe in Console" + "\n"
                 + "@author Oskar Kalbarczyk" + "\n"
                 + "Visit my gitHub at: https://github.com/oskalbarczyk" + "\n" +
@@ -28,14 +31,15 @@ public class Main {
         createBoard();
 
 
-
+    
         while (true) {
             printBoard();
+            // Check if the game is over
             if (isGameOver()) {
                 System.out.println("Game Over!");
                 break;
             }
-
+            // Player's turn
             System.out.println("Turn: " + (isOturn ? "O" : "X"));
             System.out.println("Select field you want to cover.");
             boolean correctMoveHappened = false;
@@ -46,11 +50,13 @@ public class Main {
                     System.out.println("Field already covered..." + "\n"
                                         + " Select other field.");
                 }else {
+                    // Update the selected field with the player's symbol
                     board[selectedField - 1].setSymbol(isOturn ? "O" : "X");
                     correctMoveHappened = true;
                 }
 
             }
+            // Switch turns
             isOturn = !isOturn;
         }
 
@@ -58,12 +64,19 @@ public class Main {
 
 
     }
+     /**
+     * Creates the initial game board with empty fields.
+     */
 
     public static void createBoard () {
         for (int i = 0; i < board.length; i++) {
             board[i] = new Field(" ");
         }
     }
+    
+    /**
+     * Prints the current game board.
+     */
     public static void printBoard() {
         for (int i = 0; i < board.length; i++) {
             String symbol = board[i].getSymbol();
@@ -77,6 +90,12 @@ public class Main {
             }
         }
     }
+     /**
+     * Checks if the game is over by checking for winning conditions
+     * and a full board.
+     *
+     * @return true if the game is over, false otherwise
+     */
 
     public static boolean isGameOver () {
 
@@ -126,6 +145,9 @@ public class Main {
 
 
 }
+/**
+ * Represents a field on the Tic-Tac-Toe board.
+ */
 
  class Field {
 
